bfeb97a282fefd6feccc24650a3d025b66b76104
NineCloud <gabrielarlo11@gmail.com>
2025-01-04 10:52:00 +0800

    ✨ feat(api): add customer routes
    - add RESTful routes for customer management
    - include list, create, show, update, and delete operations for customers

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', fn (Request $request) => $request->user())->middleware('auth:sanctum');
+Route::prefix('customers')->group(function (): void {
+    Route::get('/', [CustomerController::class, 'list']);
+    Route::post('/', [CustomerController::class, 'create']);
+    Route::get('/{customer}', [CustomerController::class, 'show']);
+    Route::put('/{customer}', [CustomerController::class, 'update']);
+    Route::delete('/{customer}', [CustomerController::class, 'destroy']);
+});
